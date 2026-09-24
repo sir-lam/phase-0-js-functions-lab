e976202bdea5c9d3ea9582ae908b65338c92ad5f
Abdisalam Noor <abdisallam706@gmail.com>
2026-09-24 15:30:35 +0300

    create functions in index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-
-
-
-
+function calculateTax(amount) {
+    const taxRate = 0.1; // 10% tax rate
+    return amount * taxRate;
+}
+const convertToUpperCase = (text) => {
+    return text.toUpperCase();
+}
+function findMaximum(num1, num2) {
+    return  num1 > num2 ? num1 : num2;
+}
+function isPalindrome(word) {
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
